e5be01235f8f4e2edf2ee0c5a64f30e42f0f8cc8
hmoisesgb <h.moises.gb@gmail.com>
2023-11-13 01:37:19 -0600

    Added hamburger menu and dark mode

---
+++ scripts/darkMode.js
@@ -0,0 +1,12 @@
+const modeButton = document.querySelector("#mode");
+const body = document.querySelector("body");
+const main = document.querySelector("main");
+const section = document.querySelector("section");
+const section2 = document.getElementById('section2');
+
+modeButton.addEventListener("click", () => {
+	body.classList.toggle('dark-mode');
+	main.classList.toggle('dark-mode');
+    section.classList.toggle('dark-mode');
+    section2.classList.toggle('dark-mode');
+});
