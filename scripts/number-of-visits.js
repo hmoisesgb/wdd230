e8fcff8dc3081161b93ca11aa6650cc176a494b9
hmoisesgb <h.moises.gb@gmail.com>
2023-11-17 00:49:35 -0600

    Completed W03 learning and team activity

---
+++ scripts/number-of-visits.js
@@ -0,0 +1,14 @@
+const visits = document.querySelector(".visits");
+let numVisits = Number(window.localStorage.getItem("numVisits-ls")) || 0;
+
+if (numVisits !== 0) {
+    visits.textContent = numVisits;
+
+} 
+else {
+    visits.textContent = 'This is your first visit!'
+}
+
+numVisits++;
+
+localStorage.setItem('numVisits-ls',numVisits);
---
+++ week03/scripts/bom.js
@@ -0,0 +1,57 @@
+const input = document.querySelector('#favchap');
+const button = document.querySelector('button');
+const list = document.querySelector('#list');
+
+let chaptersArray = getChapterList() || [];
+
+button.addEventListener('click', () =>{
+    if (input.value != '') {
+        displayList(input.value);
+        chaptersArray.push(input.value)
+        setChapterList();
+        input.value = '';
+        input.focus();
+    }
+    else{
+        alert("Please input a book and a chapter");
+        input.focus();
+    }
+    input.focus();
+    input.value = '';
+});
+
+chaptersArray.forEach(chapter => {
+    displayList(chapter);
+});
+
+function displayList(item) {
+    let li = document.createElement('li');
+    let deletebutton = document.createElement('button');
+    li.textContent = item;
+    deletebutton.textContent = '❌';
+    deletebutton.classList.add('delete');
+    li.append(deletebutton);
+    list.append(li);
+    deletebutton.addEventListener('click', function () {
+        list.removeChild(li);
+        deleteChapter(li.textContent);
+        input.focus();
+    })
+}
+
+function setChapterList() {
+    localStorage.setItem('myFavBOMList', JSON.stringify(chaptersArray));
+}
+
+function getChapterList() {
+    return JSON.parse(localStorage.getItem('myFavBOMList'));
+}
+
+function deleteChapter(chapter) {
+    chapter = chapter.slice(0, chapter.length - 1);
+    chaptersArray = chaptersArray.filter((item) => item !== chapter);
+    setChapterList();
+}
+
+
+
